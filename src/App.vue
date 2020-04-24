c06830da02b3c2faf0860357b2bba68bae3c72ee
luckKiven <2213593288@qq.com>
2020-04-24 10:08:43 +0800

    JSONP和CORS跨域

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,21 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
+import jsonp from 'jsonp'
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  mounted(){
+    let url = "https://www.easy-mock.com/mock/5b012c1fe6e1035843cd3aff/mockapi/table/list"
+    axios.get(url).then(()=>{
+
+    })
+    jsonp(url,()=>{
+      
+    })
   }
 }
 </script>
