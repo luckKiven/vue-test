de2e1efd64548a06f640834ba26674eb7c755d2c
luckKiven <2213593288@qq.com>
2020-04-25 17:34:43 +0800

    1

--- src/vue.config.js
+++ src/vue.config.js
@@ -6,8 +6,9 @@ modult.exports = {
             '/api':{
                 target:'http://www.tmooc.cn',
                 changeOrigin:true, //是否要将主机头的原点设置为url地址
+                'secure': false, // false为http访问，true为https访问
                 pathRewrite:{
-                    '^/api':' '
+                    '^/api':''
                 }
             }
         }
