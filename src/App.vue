68b49097f247ecfd7052d322190e054d8198dfb1
luckKiven <2213593288@qq.com>
2020-04-28 14:41:00 +0800

    项目目录结构设置提交

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <view-router></view-router>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import jsonp from 'jsonp'
-//import axios from 'axios'
+
+
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   },
   mounted(){
-    //测试cors访问,所有请求都可以访问,后端设置响应头
-    //let url = "https://www.easy-mock.com/mock/5b012c1fe6e1035843cd3aff/mockapi/table/list"
-    //axios.get(url).then(()=>{
-    //})
-    //jsonp方式访问,后端封装JSONPObject
-    //let url="https://www.imooc.com/activity/servicetime?callback=__jp0"
-    //jsonp(url,(error,res)=>{
-    //  let result =res;
-    //  this.data=result; 
-    //})
-    //代理方式访问,线上还是在nginx配置,而不应该在vue.config.js配置
-    let url="/api/activity/servicetime"
-    jsonp(url,(error,res)=>{
-      let result =res;
-      this.data=result; 
-    })
+  
   }
 }
 </script>
